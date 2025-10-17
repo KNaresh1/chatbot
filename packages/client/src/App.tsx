2b0456d8ddc85a772bc3af7c4474a6c763e1dfb5
Naresh Kakumani <nareshkakumani@gmail.com>
2025-10-17 14:19:17 -0500

    Setup Prettier

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,21 +1,21 @@
-import {useEffect, useState} from "react";
-import {Button} from "@/components/ui/button.tsx";
+import { useEffect, useState } from 'react';
+import { Button } from '@/components/ui/button.tsx';
 
 function App() {
-    const [message, setMessage] = useState('')
+   const [message, setMessage] = useState('');
 
-    useEffect(() => {
-        fetch('/api/hello')
-            .then(res => res.json())
-            .then(data => setMessage(data.message))
-    }, []);
+   useEffect(() => {
+      fetch('/api/hello')
+         .then((res) => res.json())
+         .then((data) => setMessage(data.message));
+   }, []);
 
-    return (
-        <div className="p-4">
-            <p className="font-bold text-3xl pb-4">{message}</p>
-            <Button>Click Me</Button>
-        </div>
-    );
+   return (
+      <div className="p-4">
+         <p className="font-bold text-3xl pb-4">{message}</p>
+         <Button>Click Me</Button>
+      </div>
+   );
 }
 
-export default App
+export default App;
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import type { Request, Response} from 'express';
+import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -9,13 +9,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World Server!');
+   res.send('Hello World Server!');
 });
 
 app.get('/api/hello', (req: Request, res: Response) => {
-    res.json({message: 'Hello World Client!'});
+   res.json({ message: 'Hello World Client!' });
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-})
+   console.log(`Server is running on http://localhost:${port}`);
+});
