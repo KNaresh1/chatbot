0d8abf460aa529666d10aaf6d5a7a25f23858b32
Naresh Kakumani <nareshkakumani@gmail.com>
2025-10-17 23:37:17 -0500

    Add routes, controllers, services

---
+++ packages/server/controllers/chat.controller.ts
@@ -0,0 +1,38 @@
+import type { Request, Response } from 'express';
+import { chatService } from '../services/chat.service.ts';
+import { z } from 'zod';
+
+const UUID_REGEX =
+   /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+
+const chatSchema = z.object({
+   conversationId: z.string().regex(UUID_REGEX, 'Invalid UUID'),
+   prompt: z
+      .string()
+      .trim()
+      .min(1, 'Prompt is required.')
+      .max(1000, 'Prompt is too long, max 1000 characters'),
+});
+
+export const chatController = {
+   async sendMessage(req: Request, res: Response) {
+      const parseResult = chatSchema.safeParse(req.body);
+
+      if (!parseResult.success) {
+         res.status(400).json({ errors: parseResult.error.issues });
+         return;
+      }
+
+      try {
+         const { conversationId, prompt } = req.body;
+         const { message } = await chatService.sendMessage(
+            conversationId,
+            prompt
+         );
+
+         res.json({ message });
+      } catch (error) {
+         res.status(500).json({ error: 'Failed to generate a response.' });
+      }
+   },
+};
---
+++ packages/server/services/chat.service.ts
@@ -0,0 +1,31 @@
+import { OpenAI } from 'openai';
+import { conversationRepository } from '../repositories/conversation.repository.ts';
+
+const client = new OpenAI({
+   apiKey: process.env.OPENAI_API_KEY,
+});
+
+interface ChatResponse {
+   id: string;
+   message: string;
+}
+
+export const chatService = {
+   async sendMessage(
+      conversationId: string,
+      prompt: string
+   ): Promise<ChatResponse> {
+      const response = await client.responses.create({
+         model: 'gpt-4o-mini',
+         input: prompt,
+         temperature: 0.2,
+         max_output_tokens: 100,
+         previous_response_id:
+            conversationRepository.getLastResponseId(conversationId),
+      });
+
+      conversationRepository.setLastResponseId(conversationId, response.id);
+
+      return { id: response.id, message: response.output_text };
+   },
+};
